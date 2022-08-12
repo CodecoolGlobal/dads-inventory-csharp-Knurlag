e7db862d6bc308657c33357545256ffefa09c8af
Kristóf <bkristof54@gmail.com>
2022-08-12 14:05:42 +0200

    Added services

---
+++ Services/TeamService.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using RunningActivityTracker.Entities;
+using RunningActivityTracker.Repositories;
+
+namespace RunningActivityTracker.Services
+{
+    public class TeamService : ITeamService
+    {
+
+        private ITeamRepository _repository;
+        private IUserService _userService;
+
+        public TeamService(ITeamRepository repository, IUserService userService)
+        {
+            _repository = repository;
+            _userService = userService;
+        }
+
+    public void CreateTeam(string teamName)
+    {
+            var user = _userService.FindCurrentUser();
+            List<UserEntity> members = new List<UserEntity>{user};
+            _repository.CreateTeam(teamName, members, user);
+    }
+
+        public void AddMember(string memberName)
+        {
+            UserEntity user = new UserEntity();
+
+            var users = _userService.GetAll();
+            foreach (var userPair in users)
+            {
+                if (userPair.Value.Username == memberName)
+                {
+                    user = userPair.Value;
+                    userPair.Value.Roles.Append("Member");
+                }   
+            }
+            var currentUser = _userService.FindCurrentUser();
+            var team =_repository.FindTeamByAdmin(currentUser.Username);
+            team.Members.Add(user);
+        }
+
+        public SortedList<string, TeamEntity> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using RunningActivityTracker.Entities;
+using RunningActivityTracker.Repositories;
+
+namespace RunningActivityTracker.Services
+{
+    public class UserService : IUserService
+    {
+        private IUserRepository _repository;
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IUserRepository repository, IHttpContextAccessor httpContextAccessor)
+        {
+            _repository = repository;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public UserEntity FindCurrentUser()
+        {
+            var autorizationHeader = _httpContextAccessor.HttpContext.Request.Headers.Authorization.ToString();
+            string userInfoEncoded = new string(autorizationHeader.Skip(6).ToArray()); // Remove the "Basic " start of the header value
+
+
+            string userInfoDecoded = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(userInfoEncoded));
+
+            string userName = userInfoDecoded.Split(":")[0];
+            string password = userInfoDecoded.Split(":")[1];
+            return AuthenticateAsync(userName, password).Result;
+        }
+
+        public void CreateUser(UserEntity user)
+        {
+            _repository.CreateUser(user);
+        }
+
+        public Task<UserEntity> AuthenticateAsync(string userName, string password)
+        {
+            return Task.Run(() =>
+                _repository.GetAll()
+                    .FirstOrDefault(user => user.Value.Username == userName && user.Value.Password == password).Value);
+        }
+
+        public SortedList<string, UserEntity> GetAll()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
